9dd21e114b4557f89efbb0fd007ba69bd7f6fb13
leeeeegun <leeeeegun@naver.com>
2025-09-03 17:15:05 +0900

    chore: [이의건] git check]

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -4,6 +4,7 @@ const CalendarPage = () => {
     return (
         <div className="p-8">
             <h1 className="text-2xl font-bold">캘린더</h1>
+            
             { /* 관련 UI 추가해야 함 */}
         </div>
     );
