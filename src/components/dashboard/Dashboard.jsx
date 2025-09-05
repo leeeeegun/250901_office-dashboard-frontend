83469c2497bee08ba1611123954876027fa1dcd9
leeeeegun <leeeeegun@naver.com>
2025-09-05 20:52:21 +0900

    feat: [이의건] 전사탭과 마이탭 기능 활성화 [ODF-FRONT-14]

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -5,7 +5,10 @@ import ProfileCard from './ProfileCard.jsx';
 import TodoList from './TodoList.jsx';
 
 const Dashboard = () => {
+    // 전사탭과 내탭 구분
     const [activeTab, setActiveTab] = useState('company');
+    // 근무 상태를 해당 컴포넌트에서 관리
+    const [workStatus, setWorkStatus] = useState('업무중');
 
     return (
         <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
@@ -27,23 +30,25 @@ const Dashboard = () => {
                 </div>
             </div>
 
+            {/* 전사 탭 활성화 */}
             {activeTab === 'company' ? (
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                     <div className="lg:col-span-2 space-y-6">
-                        <AttendanceCard />
+                        <AttendanceCard workStatus={workStatus} setWorkStatus={setWorkStatus} />
                         <BoardCard />
                     </div>
                     <div className="lg:col-span-1">
-                        <ProfileCard />
+                        <ProfileCard workStatus={workStatus} />
                     </div>
                 </div>
-            ) : (
+            ):(
                 <div>
                     <TodoList />
                 </div>
+
             )}
         </main>
-    );
+    )
 };
 
 export default Dashboard;
